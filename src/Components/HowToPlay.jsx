5a1564a47952a12e28fd2e408ebb2e9dc99ffe7f
Temitope <tope_olajide@outlook.com>
2020-04-25 17:53:25 +0100

    Create HowToPlay.jsx

---
+++ src/Components/HowToPlay.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useCallback } from "react";
+import {ButtonThree} from '../Components/commons/Button'
+import Nav from "../Components/Nav"
+const HomePage = () => {
+    
+  return (
+    <>
+   <Nav />
+      <section className="home-container">
+          <section className="home-title-section"><h1>How To Play</h1>
+          </section>
+          <section className="home-menu-details">
+          <p> </p>
+          </section>
+      </section>
+    </>
+  );
+};
+export default HomePage;
