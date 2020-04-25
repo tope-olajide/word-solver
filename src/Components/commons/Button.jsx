830e28ffcc874e27e8504cd2043366d965aa870a
Temitope <tope_olajide@outlook.com>
2020-04-25 17:52:52 +0100

    Create Button.jsx

---
+++ src/Components/commons/Button.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Button = ({ name, id, onClick, index }) => {
+  return (
+    <div className="button-wrapper">
+      <button style={name===" "?{"visibility":"hidden"}:{"visibility":"visible"}} className={"button"} onClick={() => onClick(name, id, index)}>
+        {name}
+      </button>
+    </div>
+  );
+};
+export const ButtonTwo = ({ name, id, onClickTwo, index }) => {
+  return (
+    <div className="button-wrapper">
+      <button key={id} className="button" onClick={() => onClickTwo(name, id, index)}>
+        {name}
+      </button>
+    </div>
+  );
+};
+
+export const ButtonThree = ({ name, onClickThree }) => {
+  return (
+    <section className="menu-button-wrapper">
+      <button className="button" onClick={onClickThree}>
+        {name}
+      </button>
+    </section>
+  );
+};
+
+export default Button;
