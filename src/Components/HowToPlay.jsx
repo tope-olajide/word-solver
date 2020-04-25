25e6e3bd620602d9129e950298b9e35c75b6cc32
Temitope <tope_olajide@outlook.com>
2020-04-26 00:42:35 +0100

    Update HowToPlay.jsx

--- src/Components/HowToPlay.jsx
+++ src/Components/HowToPlay.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import {ButtonThree} from '../Components/commons/Button'
 import Nav from "../Components/Nav"
 const HomePage = () => {
@@ -10,8 +10,11 @@ const HomePage = () => {
           <section className="home-title-section"><h1>How To Play</h1>
           </section>
           <section className="home-menu-details">
-          <p> </p>
+          <p> Create words from the letters displayed on the buttons either by clicking on them(if you are using a mobile phone or tablet), or by typing them in with your keyboard(if your using a laptop or desktop computer).</p>
+          <p>Once you've assembled a valid word, the word will be automatically added it to the word grid. The word grid might not be visible on small screen devices. Spell as many words as you can to score maximum points!(50 points per letter)</p>
+          <p>To delete a letter, click on the letter on the output grid or click on backspace on your keyboard or you can click on the clear button.</p>
           </section>
+          <ButtonThree name = "Back" />
       </section>
     </>
   );
