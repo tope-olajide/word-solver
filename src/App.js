46f016d91d7f1845271f6ff7fe9f9156f11fb7b5
Temitope <tope_olajide@outlook.com>
2020-04-25 17:52:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Game from '../src/Components/Game'
+import GameOver from '../src/Components/GameOver'
+import HomePage from '../src/Components/HomePage'
+import HowToPlay from '../src/Components/HowToPlay'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Game />
+    </>
   );
 }
 
