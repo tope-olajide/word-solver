bddef3fbe47bfc8fbc86c41b3387ed7e1016088b
Temitope <tope_olajide@outlook.com>
2020-05-01 05:30:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import GameOver from '../src/Components/GameOver'
 import HomePage from '../src/Components/HomePage'
 import HowToPlay from '../src/Components/HowToPlay'
 import SignUp from '../src/Components/SignUp'
+import LoadNextPage from './Components/LoadNextLevel'
+import Modal from './Components/Modal'
 function App() {
   return (
     <>
