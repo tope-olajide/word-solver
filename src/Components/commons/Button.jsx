087764c026eed8f25ae5df4e3fc45fd846bd9e58
Temitope <tope_olajide@outlook.com>
2020-05-05 02:31:28 +0100

    Update Button.jsx

--- src/Components/commons/Button.jsx
+++ src/Components/commons/Button.jsx
@@ -1,32 +1,62 @@
 import React from "react";
-
-const Button = ({ name, id, onClick, index }) => {
+/**
+ * InputKeyButton component -
+ * @param  {props} letter - string
+ * @param {props} id - string
+ * @param {props} selectInputButton - function
+ * @param {props} index - integer
+ * @return {object} JSX - InputKeyButton elements
+ */
+const InputKeyButton = ({ letter, id, selectInputButton, index }) => {
   return (
     <div className="button-wrapper">
-      <button style={name===" "?{"visibility":"hidden"}:{"visibility":"visible"}} className={"button"} onClick={() => onClick(name, id, index)}>
-        {name}
+      <button
+        style={!letter ? { visibility: "hidden" } : { visibility: "visible" }}
+        className={"button"}
+        onClick={() => selectInputButton(letter, id, index)}
+      >
+        {letter}
       </button>
     </div>
   );
 };
-export const ButtonTwo = ({ name, id, onClickTwo, index }) => {
+/**
+ * OutputKeyButton component
+ * @param  {props} letter - string
+ * @param {props} id - string
+ * @param {props} selectOutputButton - function
+ * @param {props} index - integer
+ * @return {object} JSX - OutputKeyButton elements
+ */
+export const OutputKeyButton = ({ letter, id, selectOutputButton, index }) => {
   return (
     <div className="button-wrapper">
-      <button key={id} className="button" onClick={() => onClickTwo(name, id, index)}>
-        {name}
+      <button
+        key={id}
+        className="button"
+        onClick={() => selectOutputButton(letter, id, index)}
+      >
+        {letter}
       </button>
     </div>
   );
 };
 
-export const ButtonThree = ({ name, onClick }) => {
+/**
+ * SecondaryButton component
+ * @param  {props} name - string
+ * @param {props} onClick - function
+ * @param {props} buttonRef - function
+ * @return {object} JSX - SecondaryButton elements
+ */
+export const SecondaryButton = ({ name, onClick, buttonRef }) => {
   return (
     <section className="menu-button-wrapper">
-      <button className="button" onClick={onClick}>
+      <button className="button" ref={buttonRef} onClick={onClick}>
         {name}
       </button>
     </section>
   );
 };
 
-export default Button;
+export default InputKeyButton;
