ced119ca32b5f6aefe6f91aabe9fc8808f9d3c3e
Temitope <tope_olajide@outlook.com>
2020-05-06 12:32:44 +0100

    Update LoadNextLevel.jsx

--- src/Components/LoadNextLevel.jsx
+++ src/Components/LoadNextLevel.jsx
@@ -1,31 +1,45 @@
-import React, { useState, useEffect, useCallback } from "react";
-import Button from "./commons/Button";
-import { ButtonTwo } from "./commons/Button";
-import Game from './Game';
-import HomePage from './HomePage'
-const Home = ({level, scores}) => {
-const currentLevel = level || 1;
-const [isGameOn, setIsGameOn] = useState(false);
-const [isHomePage, setIsHomePage] = useState(false);
-const wordsNeeded = 5 + currentLevel;
-const currentScores = scores || 0;
+import React, { useState } from "react";
+import Game from "./Game";
+import HomePage from "./HomePage";
 
-const startGame = () =>{
-  setIsGameOn(true)
-}
-const saveAndQuitGame = () => {
-  const gameStatus = {
-    currentScores, currentLevel
+/**
+ * Home component
+ * @param {props} level - integer
+ * @param {props} scores - integer
+ * @return {object} JSX - Home component elements
+ * @return {component} Homepage - renders Homepage if isHomePage = true
+ * @return {component} Game - renders Game if isHomePage = true
+ */
+const Home = ({ level, scores }) => {
+  const currentLevel = level || 1;
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [isHomePage, setIsHomePage] = useState(false);
+  const wordsNeeded = 5 + currentLevel;
+  const currentScores = scores || 0;
+
+  const startGame = () => {
+    setIsGameOn(true);
+  };
+  const saveAndQuitGame = () => {
+    const gameStatus = {
+      currentScores,
+      currentLevel,
+    };
+    localStorage.setItem("savedGame", JSON.stringify(gameStatus));
+    setIsHomePage(true);
+  };
+  if (isGameOn) {
+    return (
+      <Game
+        currentLevel={currentLevel}
+        wordsNeeded={wordsNeeded}
+        currentScores={currentScores}
+      />
+    );
   }
-  localStorage.setItem('savedGame', JSON.stringify(gameStatus));
-  setIsHomePage(true);
-}
-if(isGameOn) {
-  return <Game currentLevel={currentLevel} wordsNeeded={wordsNeeded} currentScores={currentScores}/>
-}
-if(isHomePage) {
-  return <HomePage />
-}
+  if (isHomePage) {
+    return <HomePage />;
+  }
   return (
     <>
       <section className="main-section">
@@ -37,13 +51,13 @@ if(isHomePage) {
           <section className="output-button-section">
             <div className="next-level">
               <h1 className="next-level-info">
-                Next Stage: <span>{currentLevel}</span>
+                Level: <span>{currentLevel}</span>
               </h1>
               <h1 className="next-level-info">
                 Words Needed To advance: <span> {wordsNeeded}</span>
               </h1>
               <h1 className="next-level-info">
-                Time: <span>60</span>
+                Time (seconds): <span>60</span>
               </h1>
             </div>
           </section>
