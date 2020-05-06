f32b07dd93cb56949d53ff8508cf7333b3c3ace1
Temitope <tope_olajide@outlook.com>
2020-05-06 12:32:50 +0100

    Update Modal.jsx

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-const Modal = ({modalTitle, modalBody,displayModal,modalFooter}) => {
-    return (
-        <>
-<section className="modal-section"style={displayModal?null:{display:"none"}}>
-<section className="modal-contents"> 
-<section className="modal-header"><h1>{modalTitle}</h1> </section>
-<section className="modal-body">{modalBody}
-</section>
-<section className="modal-footer">{modalFooter} </section>
-</section>
-</section>
-        </>
-    )
-}
-export default Modal
+import React from "react";
+/**
+ * Game component
+ * @param  {props} modalTitle - string
+ * @param  {props} modalBody - string
+ * @param  {props} displayModal - string
+ * @param {props} modalFooter - string
+ * @return {object} JSX - About component elements
+ */
+const Modal = ({ modalTitle, modalBody, displayModal, modalFooter }) => {
+  return (
+    <>
+      <section
+        className="modal-section"
+        style={displayModal ? null : { display: "none" }}
+      >
+        <section className="modal-contents">
+          <section className="modal-header">
+            <h1>{modalTitle}</h1>{" "}
+          </section>
+          <section className="modal-body">{modalBody}</section>
+          <section className="modal-footer">{modalFooter} </section>
+        </section>
+      </section>
+    </>
+  );
+};
+export default Modal;
